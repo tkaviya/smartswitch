37e48594046cc27c22b56bfb6c7378d7fbbf9dd3
Tsungai Kaviya <tsungai.kaviya@gmail.com>
2020-07-01 09:51:40 +0200

    Started migration to JWT Authentication

--- sym_authentication/src/main/java/net/symbiosis/authentication/provider/SymAuthenticationProvider.java
+++ sym_authentication/src/main/java/net/symbiosis/authentication/provider/SymAuthenticationProvider.java
@@ -1,39 +1,39 @@
-package net.symbiosis.authentication.provider;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-/***************************************************************************
- *                                                                         *
- * Created:     26 / 06 / 2020                                             *
- * Author:      Tsungai Kaviya                                             *
- * System:      IntelliJ 2019 / Windows 10                                 *
- * Contact:     tsungai.kaviya@gmail.com                                   *
- *                                                                         *
- ***************************************************************************/
-
-public class SymAuthenticationProvider extends GenericFilterBean {
-
-	private JwtTokenProvider jwtTokenProvider;
-	public SymAuthenticationProvider(JwtTokenProvider jwtTokenProvider) {
-		this.jwtTokenProvider = jwtTokenProvider;
-	}
-
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
-			throws IOException, ServletException {
-		String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
-		if (token != null && jwtTokenProvider.validateToken(token)) {
-			Authentication auth = token != null ? jwtTokenProvider.getAuthentication(token) : null;
-			SecurityContextHolder.getContext().setAuthentication(auth);
-		}
-		filterChain.doFilter(req, res);
-	}}
+//package net.symbiosis.authentication.provider;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.web.filter.GenericFilterBean;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+///***************************************************************************
+// *                                                                         *
+// * Created:     26 / 06 / 2020                                             *
+// * Author:      Tsungai Kaviya                                             *
+// * System:      IntelliJ 2019 / Windows 10                                 *
+// * Contact:     tsungai.kaviya@gmail.com                                   *
+// *                                                                         *
+// ***************************************************************************/
+//
+//public class SymAuthenticationProvider extends GenericFilterBean {
+//
+//	private JwtTokenProvider jwtTokenProvider;
+//	public SymAuthenticationProvider(JwtTokenProvider jwtTokenProvider) {
+//		this.jwtTokenProvider = jwtTokenProvider;
+//	}
+//
+//	@Override
+//	public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
+//			throws IOException, ServletException {
+//		String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
+//		if (token != null && jwtTokenProvider.validateToken(token)) {
+//			Authentication auth = token != null ? jwtTokenProvider.getAuthentication(token) : null;
+//			SecurityContextHolder.getContext().setAuthentication(auth);
+//		}
+//		filterChain.doFilter(req, res);
+//	}}
